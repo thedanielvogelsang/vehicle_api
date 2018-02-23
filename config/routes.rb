3aea2551e398dad31cbaf9f08edf81473b6aaf22
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-02-23 13:10:56 -0700

    prepares api routes and controllers with test-requirement actions index, show, create, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :vehicles, only: [:index, :show, :create, :update, :destroy]
+      resources :options, only: [:index, :show, :create, :update, :destroy]
+      resources :makes, only: [:index, :show, :create, :update, :destroy]
+      resources :models, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
