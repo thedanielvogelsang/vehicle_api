068d6fe125e32545eb9def1f1263733898e6eda5
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-02-23 10:12:08 -0700

    matches make and model specs

--- spec/models/make_spec.rb
+++ spec/models/make_spec.rb
@@ -8,14 +8,18 @@ RSpec.describe Make, type: :model do
     it{is_expected.to validate_presence_of(:company_desc) }
 
     it 'is created with valid attributes' do
+    #company and company_desc must be present
       invalid_1 = Make.new(company: 'Honda', company_motto: 'good products come from clean workplaces')
       invalid_2 = Make.new(ceo_statement: 'Hello', company_motto: 'good products come from clean workplaces')
       valid_make = Make.new(company: 'Honda', company_desc: DESC)
-      
       expect(invalid_1.class).to be(Make)
       expect(invalid_1.save).to be false
       expect(invalid_2.save).to be false
       expect(valid_make.save).to be true
+
+    # name uniqueness
+      invalid_3 = Make.new(company: 'Honda', company_desc: DESC)
+      expect(invalid_3.save).to be false
     end
   end
 end
