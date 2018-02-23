856bc588151a3cd24ce8c6d0160d7b7f497de8b2
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-02-23 09:52:46 -0700

    adds valid attributes save confirmation happy and sad paths

--- spec/models/make_spec.rb
+++ spec/models/make_spec.rb
@@ -3,10 +3,19 @@ require_relative './honda_description'
 
 RSpec.describe Make, type: :model do
   context 'validations' do
-    invalid_1 = Make.new(company: 'Honda', company_motto: 'good products come from clean workplaces')
-    invalid_2 = Make.new(ceo_statement: 'Hello', company_motto: 'good products come from clean workplaces')
-    valid_make = Make.new(company: 'Honda', company_desc: DESC)
+
     it{is_expected.to validate_presence_of(:company) }
     it{is_expected.to validate_presence_of(:company_desc) }
+
+    it 'is created with valid attributes' do
+      invalid_1 = Make.new(company: 'Honda', company_motto: 'good products come from clean workplaces')
+      invalid_2 = Make.new(ceo_statement: 'Hello', company_motto: 'good products come from clean workplaces')
+      valid_make = Make.new(company: 'Honda', company_desc: DESC)
+      
+      expect(invalid_1.class).to be(Make)
+      expect(invalid_1.save).to be false
+      expect(invalid_2.save).to be false
+      expect(valid_make.save).to be true
+    end
   end
 end
