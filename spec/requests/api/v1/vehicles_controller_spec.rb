1419980b720123051e8f105a7498ff3aa929ffda
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-02-24 20:40:49 -0700

    passing sad path for post to vehicles, ready for Make, Model and Options

--- spec/requests/api/v1/vehicles_controller_spec.rb
+++ spec/requests/api/v1/vehicles_controller_spec.rb
@@ -39,11 +39,21 @@ describe 'vehicles_api' do
     post "/api/v1/vehicles?model_id=#{mId}&make_id=#{maId}&vin=Ab9kl7"
 
     expect(response).to be_success
+    expect(response.status).to eq(201)
     expect(Vehicle.count).to eq(4)
 
     vehicle = JSON.parse(response.body)
     expect(vehicle['make']).to eq('Honda')
     expect(vehicle['model']).to eq('Civic')
     expect(vehicle['vin']).to eq('Ab9kl7')
+  end
+  it 'will reject a post with improper params' do
+    expect(Vehicle.count).to eq(3)
+    mId = Model.last.id
+    post "/api/v1/vehicles?model_id=#{mId}&make_id=abc123&vin=Ab9kl7"
+
+    expect(response).to_not be_success
+    expect(response.status).to eq(400)
+    expect(Vehicle.count).to eq(3)
   end
 end
